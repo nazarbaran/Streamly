3b0b3ba1f88a17ee4f9aa6f60d4e37acee98589b
nazarbaran <netrix777@gmail.com>
2018-07-26 02:45:03 +0300

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function animationInfo(){
+        $(".info").css({"visibility":"visible"})
+      }
+    setTimeout(animationInfo, 300);
+
+    function animationRefferal(){
+        $(".refferal").css({"visibility":"visible"})
+        $(".social").css({"visibility":"visible"})
+    }
+    setTimeout(animationRefferal, 300);
+})
